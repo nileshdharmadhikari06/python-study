46a93721a2447500fd8087865cb898c0aacaae20
Nilesh Dharmadhikari <nilesh.dharmadhikari06@gmail.com>
2026-04-26 19:52:55 +0530

    Initial commit

---
+++ friday.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    returnx+y
+    
+def even_odd(x):
+    if x%2==0:
+        print("even")
+    else:
+        print("odd")
+
+def fact(x):
+    fac=1
+    for i in range(1,x+1):
+        fzc=fzc*i
+    return fac
+
+def sum_all(x):
+    add=0
+list()
---
+++ untitled.py
@@ -0,0 +1,8 @@
+def even_odd(x):
+    if x%2==0:
+        print("even")
+    else:
+        print("odd")
+
+def fact(x):
+    fac=1
